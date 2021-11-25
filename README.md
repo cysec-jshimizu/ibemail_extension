6c02630a8d079854e5f09e62bc5f915f019e8f21
cysec-jshimizu <77430623+cysec-jshimizu@users.noreply.github.com>
2021-11-25 17:26:35 +0900

    v1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ibemail_extension
+
+ibemailの復号，検証結果をgmailで表示する拡張機能
---
+++ js/sample.js
@@ -0,0 +1,58 @@
+function mailParser(email) {
+    let headerStr = email.substr(0, email.indexOf("\n\n"));
+    let headerList = {};
+    let revHeader = headerStr.split("\n").reverse();
+
+    revHeader.forEach((line, index) => {
+        if (line[0] === " ") {
+            revHeader[index + 1] += line.trim();
+        } else {
+            colon = line.indexOf(":");
+            let tag = line.substr(0, colon);
+            let content = line.substr(colon + 2);
+            if (headerList[tag]) {
+                headerList[tag].push(content);
+            } else {
+                headerList[tag] = [content];
+            }
+        }
+    });
+
+    return headerList;
+}
+
+function insertTable(table, tag, value) {
+    let tr = document.createElement("tr");
+    table.appendChild(tr);
+
+    let th = document.createElement("th");
+    th.innerHTML = tag + ":";
+    tr.appendChild(th);
+
+    let td = document.createElement("td");
+    td.innerHTML = value;
+    tr.appendChild(td);
+}
+
+
+function main() {
+    console.log("gmail source loaded");
+    let rawEmail = document.getElementById("raw_message_text").innerHTML;
+    let parsed = mailParser(rawEmail);
+    console.table(parsed);
+
+    const table = document.querySelector(".top-area table tbody");
+    const ibeDecrypted = "Ibemail-Decrypted"
+    const ibeVerify = "IBE-Verify"
+
+    if (parsed[ibeDecrypted] && parsed[ibeDecrypted].length === 1) {
+        // console.log("Decrypt:", parsed["Ibemail-Decrypted"][0]);
+        insertTable(table, ibeDecrypted, parsed[ibeDecrypted][0]);
+    }
+    if (parsed[ibeVerify] && parsed[ibeVerify].length === 1) {
+        // console.log("Verify:", parsed["IBE-Verify"][0]);
+        insertTable(table, ibeVerify, parsed[ibeVerify][0]);
+    }
+}
+
+main();
